338854779efb61e24877d83a2ef3b70598427b97
柳井 <108969330+Yanai1005@users.noreply.github.com>
2025-08-14 00:48:37 +0900

    [add] 基本的なルーティング

---
+++ app/routes/lending-status.tsx
@@ -0,0 +1,15 @@
+import type { MetaFunction } from "@remix-run/cloudflare";
+
+export const meta: MetaFunction = () => {
+  return [{ title: "貸し出し状況 | 貸し借り管理システム" }];
+};
+
+export default function LendingStatus() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <h1 className="text-3xl font-bold text-gray-800">
+        貸し出し状況 | 貸し借り管理システム
+      </h1>
+    </div>
+  );
+}
---
+++ app/routes/lending.tsx
@@ -0,0 +1,15 @@
+import type { MetaFunction } from "@remix-run/cloudflare";
+
+export const meta: MetaFunction = () => {
+  return [{ title: "貸し出し登録 | 貸し借り管理システム" }];
+};
+
+export default function Lending() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <h1 className="text-3xl font-bold text-gray-800">
+        貸し出し | 貸し借り管理システム
+      </h1>
+    </div>
+  );
+}
