77856bad772309f7b2dc2cd8e2b309e4e1f629a0
柳井 <108969330+Yanai1005@users.noreply.github.com>
2025-08-15 04:10:22 +0900

    [clean] prettierを実行

--- app/routes/lending.tsx
+++ app/routes/lending.tsx
@@ -1,9 +1,9 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
-import Navbar from "~/components/navbar";
+import type { MetaFunction } from '@remix-run/cloudflare'
+import Navbar from '~/components/navbar'
 
 export const meta: MetaFunction = () => {
-  return [{ title: "貸し出し登録 | 貸し借り管理システム" }];
-};
+  return [{ title: '貸し出し登録 | 貸し借り管理システム' }]
+}
 
 export default function Lending() {
   return (
@@ -13,5 +13,5 @@ export default function Lending() {
       </h1>
       <Navbar />
     </div>
-  );
+  )
 }
